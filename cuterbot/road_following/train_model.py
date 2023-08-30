7a2ab9d3b35df80e02eac4b0cd72fb01b87dbe7c
cuter9 <cuter9@msn.com>
2023-08-31 03:12:36 +0800

    update

--- cuterbot/road_following/train_model.py
+++ cuterbot/road_following/train_model.py
@@ -30,7 +30,7 @@ os.makedirs(DIR_DATA_REPO_PROJECT, exist_ok=True)
 DIR_DATA_REPO_THIS = os.path.join(DIR_DATA_REPO_PROJECT, "road_following")
 os.makedirs(DIR_DATA_REPO_THIS, exist_ok=True)
 
-TRAIN_MODEL = "resnet101"        # resnet18, resnet34, resnet50, resnet101
+TRAIN_MODEL = "resnet50"        # resnet18, resnet34, resnet50, resnet101
 # *** refererence : https://pytorch.org/docs/stable/optim.html#algorithms
 # use the following learning algorithms for evaluation
 TRAIN_MATHOD = "Adam"       # "Adam", "SGD", "ASGD", "Adagrad"; the parameters lr=0.01, momentum=0.92  may be needed
@@ -251,6 +251,7 @@ mean_lt = np.mean(learning_time)
 max_lt = np.amax(learning_time)
 min_lt = np.amax(learning_time)
 print("mean learning time: {:.3f} s, maximum learning time: {:.3f} s, minimum learning time: {:.3f} s".format(mean_lt, max_lt, min_lt))
+plt.savefig("Training convergence plot_Model : {:s}_Training Method : {:s})".format(TRAIN_MODEL, TRAIN_MATHOD))
 # Once the model is trained, it will generate ``best_steering_model_xy_resnet34.pth`` file which you can use for
 # inferencing in the live demo notebook.
 # 
