17982c16f81b14b79d95c66b7db4c066a7cae074
cuter9 <cuter9@msn.com>
2023-09-20 19:41:10 +0800

    Merge remote-tracking branch 'origin/master'

--- jetbot/__init__.py
+++ jetbot/__init__.py
@@ -5,4 +5,4 @@ from .robot import Robot
 from .image import bgr8_to_jpeg
 from .object_detection import ObjectDetector
 from .object_follower import Object_Follower
-from .object_detection_yolo import ObjectDetector_YOLO
+# from .object_detection_yolo import ObjectDetector_YOLO
