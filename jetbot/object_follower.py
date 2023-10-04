04f9db59a99f6c77c060a0fa9fa800d4e4caee76
cuter9 <cuter9@msn.com>
2023-10-04 17:29:32 +0800

    Merge remote-tracking branch 'origin/master'

--- jetbot/object_follower.py
+++ jetbot/object_follower.py
@@ -36,13 +36,14 @@ from jetbot import Robot
 from jetbot import bgr8_to_jpeg
 import time
 import threading
-    # 
-    # model = ObjectDetector('ssd_mobilenet_v2_coco_onnx.engine')
-    # model = ObjectDetector_YOLO('yolov4-288.engine')
 
 
-class ObjectFollower(traitlets.HasTraits): 
-    
+#
+# model = ObjectDetector('ssd_mobilenet_v2_coco_onnx.engine')
+# model = ObjectDetector_YOLO('yolov4-288.engine')
+
+
+class ObjectFollower(traitlets.HasTraits):
     cap_image = traitlets.Any()
     label = traitlets.Integer(default_value=1).tag(config=True)
     speed = traitlets.Float(default_value=0.15).tag(config=True)
@@ -50,8 +51,9 @@ class ObjectFollower(traitlets.HasTraits):
     steering_bias = traitlets.Float(default_value=0.0).tag(config=True)
     blocked = traitlets.Float(default_value=0).tag(config=True)
     is_dectecting = traitlets.Bool(default_value=True).tag(config=True)
-    
-    def __init__(self, follower_model='ssd_mobilenet_v2_coco_onnx.engine', avoider_model='../collision_avoidance/best_model.pth', type_follower_model="SSD"):
+
+    def __init__(self, follower_model='ssd_mobilenet_v2_coco_onnx.engine',
+                 avoider_model='../collision_avoidance/best_model.pth', type_follower_model="SSD"):
         self.follower_model = follower_model
         self.avoider_model = avoider_model
 
@@ -62,7 +64,7 @@ class ObjectFollower(traitlets.HasTraits):
         # elif type_model == "YOLO":
         #    from jetbot.object_detection_yolo import ObjectDetector_YOLO
         #    self.object_detector = ObjectDetector_YOLO(self.follower_model)
-        
+
         self.robot = Robot()
         self.detections = None
         self.matching_detections = None
@@ -76,14 +78,13 @@ class ObjectFollower(traitlets.HasTraits):
         self.img_height = self.capturer.height
         self.cap_image = np.empty((self.img_height, self.img_width, 3), dtype=np.uint8).tobytes()
 
-        
     def run_follower_detection(self):
         # self.image = self.capturer.value
         # print(self.image[1][1], np.shape(self.image))
         self.detections = self.object_detector(self.current_image)
         self.matching_detections = [d for d in self.detections[0] if d['label'] == int(self.label)]
         # print(int(self.label), "\n", self.matching_detections)
-        
+
     def object_center_detection(self, det):
         """Computes the center x, y coordinates of the object"""
         # print(self.matching_detections)
@@ -92,7 +93,7 @@ class ObjectFollower(traitlets.HasTraits):
         center_y = (bbox[1] + bbox[3]) / 2.0 - 0.5
         object_center = (center_x, center_y)
         return object_center
-    
+
     def norm(self, vec):
         """Computes the length of the 2D vector"""
         return np.sqrt(vec[0] ** 2 + vec[1] ** 2)
@@ -104,16 +105,17 @@ class ObjectFollower(traitlets.HasTraits):
             for det in self.matching_detections:
                 if closest_detection is None:
                     closest_detection = det
-                elif self.norm(self.object_center_detection(det)) < self.norm(self.object_center_detection(closest_detection)):
+                elif self.norm(self.object_center_detection(det)) < self.norm(
+                        self.object_center_detection(closest_detection)):
                     closest_detection = det
-        
-        self.closest_object =  closest_detection
-        
-    def start_run(self):
+
+        self.closest_object = closest_detection
+
+    def start_run(self, change):
         self.capturer.unobserve_all()
         print("start running")
         self.capturer.observe(self.execute, names='value')
- 
+
     def execute(self, change):
         # print("start excution !")
         self.current_image = change['new']
@@ -126,7 +128,7 @@ class ObjectFollower(traitlets.HasTraits):
         # self.blocked = self.obstacle_detector.prob_blocked
         # turn left if blocked
         if self.blocked > 0.5:
-        #      # robot.left(0.3)
+            #      # robot.left(0.3)
             self.robot.left(0.05)
             self.cap_image = bgr8_to_jpeg(self.current_image)
             return
@@ -136,10 +138,9 @@ class ObjectFollower(traitlets.HasTraits):
         self.closest_object_detection()
         # detections = self.object_detector(image)
         # print(self.detections)
-        
+
         # draw all detections on image
         for det in self.detections[0]:
-            
             bbox = det['bbox']
             cv2.rectangle(self.current_image, (int(width * bbox[0]), int(height * bbox[1])),
                           (int(width * bbox[2]), int(height * bbox[3])), (255, 0, 0), 2)
@@ -160,7 +161,7 @@ class ObjectFollower(traitlets.HasTraits):
         # otherwise steer towards target
         else:
             # move robot forward and steer proportional target's x-distance from center
-            center =self.object_center_detection(cls_obj)
+            center = self.object_center_detection(cls_obj)
             self.robot.set_motors(
                 float(self.speed + self.turn_gain * center[0] + self.steering_bias),
                 float(self.speed - self.turn_gain * center[0] + self.steering_bias)
@@ -171,9 +172,8 @@ class ObjectFollower(traitlets.HasTraits):
         self.cap_image = bgr8_to_jpeg(self.current_image)
         # print("ok!")
         # return self.cap_image
-        
 
-    def stop_run(self):
+    def stop_run(self, change):
         # with out:
         print("start stopping!")
         self.capturer.unobserve_all()
@@ -182,7 +182,7 @@ class ObjectFollower(traitlets.HasTraits):
 
 
 class Avoider(object):
-    
+
     def __init__(self, model_params='../collision_avoidance/best_model.pth'):
         self.model_params = model_params
         self.collision_model = torchvision.models.alexnet(pretrained=False)
@@ -192,13 +192,11 @@ class Avoider(object):
         self.device = torch.device('cuda')
         self.collision_model = self.collision_model.to(self.device)
         self.prob_blocked = 0
-        
+
     def detect(self, image):
         collision_output = self.collision_model(self.preprocess(image)).detach().cpu()
         self.prob_blocked = float(F.softmax(collision_output.flatten(), dim=0)[0])
         # blocked_widget.value = prob_blocked
-        
-
 
     def preprocess(self, camera_value):
         # global device
@@ -214,5 +212,3 @@ class Avoider(object):
         x = x.to(self.device)
         x = x[None, ...]
         return x
-
-
