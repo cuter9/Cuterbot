ba2b55e0bf09ab752c9e948d13b4967f07ec7453
cuter <cuter9@msn.com>
2025-03-02 22:53:42 +0800

    Merge remote-tracking branch 'origin/master'

--- jetbot/utils/fish_cam_calibration.py
+++ jetbot/utils/fish_cam_calibration.py
@@ -45,11 +45,12 @@ for path in glob.glob(images):
     print(ret, str(path))
     counter += 1
 
-N_imm = counter  # number of calibration images
+cal_dim = img.shape[:2]   # dimension of image for calibration
+N_OK = counter  # number of applicable calibration images
 K = np.zeros((3, 3))
 D = np.zeros((4, 1))
-rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_imm)]
-tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_imm)]
+rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
 rms, _, _, _, _ = cv2.fisheye.calibrate(
     objpoints,
     imgpoints,
@@ -61,28 +62,44 @@ rms, _, _, _, _ = cv2.fisheye.calibrate(
     calibration_flags,
     (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6))
 
+print("Found " + str(N_OK) + " valid images for calibration")
+print("DIM=" + str(cal_dim))
+print("K=np.array(" + str(K.tolist()) + ")")
+print("D=np.array(" + str(D.tolist()) + ")")
+
 ''' 焦距比例控制，控制视场大小
+    https://medium.com/@kennethjiang/calibrate-fisheye-lens-using-opencv-part-2-13990f1b157f
     https://blog.csdn.net/guanjing_dream/article/details/133576214?spm=1001.2014.3001.5502
     https://tencentcloud.csdn.net/67888801edd0904849a56cf4.html?dp_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MjQ3Nzc3MywiZXhwIjoxNzQxNDIzMDM2LCJpYXQiOjE3NDA4MTgyMzYsInVzZXJuYW1lIjoiY3V0ZXI5In0.BrzEl5uyk_Piwtd93gFn0qYMpD5DJ3bEOqjOmf3BXx4&spm=1001.2101.3001.6650.8&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7Eactivity-8-134042387-blog-133576214.235%5Ev43%5Epc_blog_bottom_relevance_base5&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7Eactivity-8-134042387-blog-133576214.235%5Ev43%5Epc_blog_bottom_relevance_base5&utm_relevant_index=12
-'''
-fs = 1.5
-newK = K.copy()
-newK[0][2] = fs * newK[0][2]
-newK[1][2] = fs * newK[1][2]
 
+# fs = 1.5    # 2.0
+# newK = K.copy()
+# newK[0][2] = fs * newK[0][2]
+# newK[1][2] = fs * newK[1][2]
+'''
 dir_lane_img = 'D:\AI_Lecture_Demos\Data_Repo\Cuterbot_Repo\lane_dataset\images'
 test_image_name = "xy_180_319_592f55da-c27a-11ef-82a3-7cb27d304b9d.jpg"
 # test_image = os.path.join(dir_lane_img, test_image_name)
 test_image = os.path.join(dir_lane_img, "*.jpg")
 
-
+balance = 0.8       # [0, 1]
 for tm in glob.glob(test_image):
-    test_img_org = cv2.imread(tm)
-    border_x, border_y = int(test_img_org.shape[0]/4), int(test_img_org.shape[1]/4)
-    width, height = (int(test_img_org.shape[0] + test_img_org.shape[0]/2),
+    test_img = cv2.imread(tm)
+    dim1 = test_img.shape[:2][::-1]  # dim1 is the dimension of input image to un-distort
+    ''' border_x, border_y = int(test_img_org.shape[0]/4), int(test_img_org.shape[1]/4)     # x/2
+    width, height = (int(test_img_org.shape[0] + test_img_org.shape[0]/2),      #x/1
                      int(test_img_org.shape[1] + test_img_org.shape[1]/2))
-    test_img = cv2.copyMakeBorder(test_img_org, border_y, border_y, border_x, border_x, cv2.BORDER_ISOLATED)
-    map1, map2 = cv2.fisheye.initUndistortRectifyMap(newK, D, np.eye(3), newK, (width, height), cv2.CV_16SC2)
+    '''
+    scaled_K = K * dim1[0] / cal_dim[0]  # The values of K are to scale with calibration image dimension.
+    scaled_K[2][2] = 1.0  # Except that K[2][2] is always 1.0
+    dim2 = dim1     # dim of input image for un-distort
+    dim3 = dim1     # dim of input image for final un-distort image
+    '''This is how scaled_K, dim2 and balance are used to determine the final K used to un-distort image. 
+    OpenCV document failed to make this clear!
+    '''
+    new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, D, dim2, np.eye(3), balance=balance)
+    # test_img = cv2.copyMakeBorder(test_img_org, border_y, border_y, border_x, border_x, cv2.BORDER_ISOLATED)
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
     undistorted_img = cv2.remap(test_img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
 
     cv2.namedWindow('Original Image', cv2.WINDOW_NORMAL)
