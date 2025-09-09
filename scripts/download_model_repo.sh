f4af280df08193e8f3e4c121eb813fe6dfaec6f8
Cuter <cuter9@msn.com>
2025-09-09 21:36:16 +0800

    Merge remote-tracking branch 'origin/master'

--- scripts/download_model_repo.sh
+++ scripts/download_model_repo.sh
@@ -1,6 +1,6 @@
 sudo apt install -y python3-pip
 sudo pip3 install -U pip gdown
-sudo chown -R $(whoami) "/home/cuterbot/.cache/"
+sudo chown -R $(whoami) $HOME/.cache/
 sudo cp ./gdrive_model_repo_cookies.json $HOME/.cache/gdown/cookies.json
 # sudo cp ./gdrive_model_repo_cookies.txt $HOME/.cache/gdown/cookies.txt
 gdown --no-cookies --folder https://drive.google.com/drive/folders/1-RIQ6lSdFI94IMlZbnRPMQHcG4L8t9Ss -O ${HOME}/model_repo
