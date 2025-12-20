23c6df45b9028b92f35ed4174dcc3fc1a4a0ff7f
cuter9 <cuter9@msn.com>
2025-12-20 22:09:25 +0800

    revise code

--- test/file_copy.sh
+++ test/file_copy.sh
@@ -3,7 +3,15 @@
 cp /workspace/Cuterbot_Demo/jetbot/camera/opencv_gst_camera.py /usr/local/lib/python3.6/dist-packages/jetbot/camera/
 nano /usr/local/lib/python3.6/dist-packages/jetbot/camera/opencv_gst_camera.py
 
+cp /workspace/Cuterbot_Demo/jetbot/data_collection.py /usr/local/lib/python3.6/dist-packages/jetbot/
+nano /usr/local/lib/python3.6/dist-packages/jetbot/data_collection.py
+
 cp /workspace/Cuterbot_Demo/jetbot/object_follower.py /usr/local/lib/python3.6/dist-packages/jetbot/
 nano /usr/local/lib/python3.6/dist-packages/jetbot/object_follower.py
+
+cp /workspace/Cuterbot_Demo/jetbot/__init__.py /usr/local/lib/python3.6/dist-packages/jetbot/
+nano /usr/local/lib/python3.6/dist-packages/jetbot/__init__.py
+
+
 
 
