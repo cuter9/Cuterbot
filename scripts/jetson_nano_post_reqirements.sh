2ea8cfe4a795a1ba2c2a86c1be1f99c5e2151044
cuter9 <cuter9@msn.com>
2025-12-22 15:17:32 +0800

    revise code

--- scripts/jetson_nano_post_reqirements.sh
+++ scripts/jetson_nano_post_reqirements.sh
@@ -12,36 +12,45 @@ cd
 echo -e "\e[104m Install pip and some python dependencies \e[0m"
 sudo apt update
 # sudo apt install -y python3-pip python3-pil
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip pkg-config
 sudo -H python3 -m pip install pip -U
-sudo -H python3 -m pip install pillow --force-reinstall
-sudo -H python3 -m pip install "Cython<3"
-sudo -H python3 -m pip install --upgrade "numpy<1.19.0"
+# sudo -H python3 -m pip install pillow --force-reinstall
+sudo pip3 install wget
 
 # Install jtop
 echo -e "\e[100m Install jtop \e[0m"
 sudo -H python3 -m pip install jetson-stats 
 
+mkdir -p $HOME/Downloads
 
 # Install the pre-built TensorFlow pip wheel
 echo -e "\e[48;5;202m Install the pre-built TensorFlow pip wheel \e[0m"
 sudo apt update
-sudo apt install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
-# sudo apt-get install -y python3-pip
-sudo -H python3 -m pip install -U testresources setuptools numpy==1.18.5 future==0.17.1 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.3.3 futures==3.1.1 "protobuf==4.21.0" pybind11
-# sudo -H python3 -m pip install -U pip testresources setuptools numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 protobuf pybind11
 # TF-1.15
-sudo -H python3 -m pip install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v461 'tensorflow<2'
+# sudo -H python3 -m pip install "Cython<3"
+# sudo -H python3 -m pip install --upgrade "numpy<1.19.0"
+# sudo apt install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+# sudo apt-get install -y python3-pip
+# sudo -H python3 -m pip install -U testresources setuptools numpy==1.18.5 future==0.17.1 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.3.3 futures==3.1.1 "protobuf==4.21.0" pybind11
+# sudo -H python3 -m pip install -U pip testresources setuptools numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 protobuf pybind11
+# sudo -H python3 -m pip install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v461 'tensorflow<2'
+
+# TF-2.7
+cd $HOME/Downloads
+sudo pip3 uninstall -y numpy
+sudo apt-get install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
+sudo pip3 install --verbose 'protobuf<4' 'Cython<3'
+sudo wget --no-check-certificate https://developer.download.nvidia.com/compute/redist/jp/v461/tensorflow/tensorflow-2.7.0+nv22.1-cp36-cp36m-linux_aarch64.whl
+sudo pip3 install --verbose tensorflow-2.7.0+nv22.1-cp36-cp36m-linux_aarch64.whl --force-reinstall
 
 # Install the pre-built PyTorch pip wheel
 # https://forums.developer.nvidia.com/t/pytorch-for-jetson/72048
 echo -e "\e[45m Install the pre-built PyTorch pip wheel  \e[0m"
-
-mkdir -p $HOME/Downloads
-
-cd $HOME/Downloads
+sudo pip3 install matplotlib
+sudo pip3 install pillow --force-reinstall
+# cd $HOME/Downloads
 sudo apt install -y libopenblas-base libopenmpi-dev
-sudo pip3 install wget
 # wget -N https://nvidia.box.com/shared/static/yr6sjswn25z7oankw8zy1roow9cy5ur1.whl -O torch-1.6.0rc2-cp36-cp36m-linux_aarch64.whl
 wget -N https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
 sudo -H python3 -m pip install torch-1.10.0-cp36-cp36m-linux_aarch64.whl 
