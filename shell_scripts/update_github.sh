ecb484d0914c72d74af1b3773fee3d32264c4517
Cuter <cuter9@msn.com>
2026-08-22 17:12:21 +0800

    a new fleet control with only one model; flee control

--- shell_scripts/update_github.sh
+++ shell_scripts/update_github.sh
@@ -6,7 +6,7 @@ cd /home/cuterbot/Cuterbot
 sudo git pull origin v2.01
 sudo git reset --hard origin/v2.01
 # shellcheck disable=SC1065
-sleep 10
+sleep 5
 # shellcheck disable=SC1072
 sudo chmod +x "/home/cuterbot/Cuterbot/shell_scripts/update_github.sh"
 sleep 5
