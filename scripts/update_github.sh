254fa87c9b1d9bbbf115d9f62eff0bdd8bd89880
Cuter <cuter9@msn.com>
2026-08-23 23:29:23 +0800

    a new fleet control with only one model; flee control

--- scripts/update_github.sh
+++ scripts/update_github.sh
@@ -8,13 +8,13 @@ sudo git reset --hard origin/v2.01
 # shellcheck disable=SC1065
 sleep 5
 # shellcheck disable=SC1072
-sudo chmod +x "/home/cuterbot/Cuterbot/shell_scripts/update_github.sh"
+sudo chmod +x "/home/cuterbot/Cuterbot/scripts/update_github.sh"
 sleep 5
 # shellcheck disable=SC2046
-sudo chown $(whoami) "/home/cuterbot/Cuterbot/shell_scripts/fix_files.sh"
+sudo chown $(whoami) "/home/cuterbot/Cuterbot/scripts/fix_files.sh"
 sudo chmod +x  "$HOME"/Cuterbot/shell_scripts/fix_files.sh
 # shellcheck disable=SC1001
-sudo "/home/cuterbot/Cuterbot/shell_scripts/fix_files.sh"
+sudo "/home/cuterbot/Cuterbot/scripts/fix_files.sh"
 # shellcheck disable=SC2164
 cd "$HOME"
 
