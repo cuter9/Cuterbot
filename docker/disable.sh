78d7ebf21b0edcb5c939324cb233dbce247f4179
Cuter <cuter9@msn.com>
2026-08-30 00:37:28 +0800

    a new fleet control with only one model; flee control

--- docker/disable.sh
+++ docker/disable.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
 
+# shellcheck disable=SC2164
+cd "$HOME"/Cuterbot/docker
 source configure.sh
 
-JUPYTER_WORKSPACE=${1:-$HOME}  # default to $HOME
-JETBOT_CAMERA=${2:-opencv_gst_camera}  # default to opencv
+JETBOT_CAMERA=${1:-opencv_gst_camera}  # default to opencv
 
 if [ "$JETBOT_CAMERA" = "zmq_camera" ]; then
   sudo docker stop jetbot_camera
   sudo docker rm jetbot_camera
 fi
 
-sudo docker stop jetbot_jupyter 
+sudo docker stop jetbot_jupyter
 sudo docker rm jetbot_jupyter
 
-sudo docker stop jetbot_display 
+sudo docker stop jetbot_display
 sudo docker rm jetbot_display
