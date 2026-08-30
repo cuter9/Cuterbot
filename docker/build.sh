47a81953e595796a225574dd4727732768d414ef
Cuter <cuter9@msn.com>
2026-08-30 11:19:09 +0800

    a new fleet control with only one model; flee control

--- docker/build.sh
+++ docker/build.sh
@@ -6,13 +6,18 @@ LOG_FILE="$HOME"/Cuterbot/docker/docker_build_.log
 
 # shellcheck disable=SC2164
 cd "$HOME"/Cuterbot/docker
-source ./configure.sh >> $LOG_FILE
-cd base && ./build.sh && cd .. >> $LOG_FILE
-cd jetbot && ./build.sh && cd .. >> $LOG_FILE
+source ./configure.sh >> $LOG_FILE 2>&1
+# docker images --format "{{.Repository}}:{{.Tag}}" | grep -q ":base.*"
+base_img=$(docker images --format "{{.Tag}}" | grep "^base.*") >> $LOG_FILE 2>&1
+if [[ -z "$base_img" ]]; then
+  (cd base && ./build.sh) >> $LOG_FILE 2>&1
+fi
+# shellcheck disable=SC2129
+(cd jetbot && ./build.sh) >> $LOG_FILE 2>&1
 # cd models && ./build.sh && cd .. >> $LOG_FILE
-cd display && ./build.sh && cd .. >> $LOG_FILE
-cd jupyter && ./build.sh && cd .. >> $LOG_FILE
-cd camera && ./build.sh && cd .. >> $LOG_FILE
+(cd display && ./build.sh) >> $LOG_FILE 2>&1
+(cd jupyter && ./build.sh)>> $LOG_FILE 2>&1
+(cd camera && ./build.sh)>> $LOG_FILE 2>&1
 
 ./disable.sh
 docker image prune -f
