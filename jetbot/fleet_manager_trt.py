6c2718c17f430c5c82768e33ae1626c31bceec6c
Cuter <cuter9@msn.com>
2026-08-30 19:51:39 +0800

    a new fleet control with only one model; flee control

--- jetbot/fleet_manager_trt.py
+++ jetbot/fleet_manager_trt.py
@@ -98,12 +98,11 @@ class FleeterTRT(ObjectFollower, RoadCruiserTRT):
         # self.execution_time.append(end_time - start_time + self.capturer.cap_time)
         self.execution_time_fm.append(end_time - start_time)
         # self.fps.append(1/(end_time - start_time))
-        # self.execute_rc(change)
 
         # if the closest object is not detected and followed, perform the road cruising
-        # if not self.is_detected:
-        #    self.execute_rc(change)
-        #    self.speed_fm = self.speed_rc  # set fleet mge speed to road cruising speed (self.speed)
+        if not self.is_detected:
+            self.execute_rc(change)
+            self.speed_fm = self.speed_rc  # set fleet mge speed to road cruising speed (self.speed)
 
     def start_fm(self, change):
         self.capturer.unobserve_all()
@@ -112,9 +111,9 @@ class FleeterTRT(ObjectFollower, RoadCruiserTRT):
 
         print("start running!")
         self.capturer.observe(self.execute_fm, names='value')
-        self.capturer.observe(self.execute_rc, names='value')
-        if not self.is_detected:
-            self.speed_fm = self.speed_rc  # set fleet mge speed to road cruising speed (self.speed)
+        # self.capturer.observe(self.execute_rc, names='value')
+        # if not self.is_detected:
+        #    self.speed_fm = self.speed_rc  # set fleet mge speed to road cruising speed (self.speed)
 
     def execute(self, change):
         # print("start execution !")
