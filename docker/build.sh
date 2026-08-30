bac5fb0cf8755a3201c4f868d39c474547e9fc4d
Cuter <cuter9@msn.com>
2026-08-30 12:24:39 +0800

    a new fleet control with only one model; flee control

--- docker/build.sh
+++ docker/build.sh
@@ -6,18 +6,20 @@ LOG_FILE="$HOME"/Cuterbot/docker/docker_build_.log
 
 # shellcheck disable=SC2164
 cd "$HOME"/Cuterbot/docker
-source ./configure.sh >> $LOG_FILE 2>&1
+source ./configure.sh >> $LOG_FILE
 # docker images --format "{{.Repository}}:{{.Tag}}" | grep -q ":base.*"
 base_img=$(docker images --format "{{.Tag}}" | grep "^base.*") >> "$LOG_FILE" 2>&1
-if [[ -z "$base_img" ]]; then
-  (cd base && ./build.sh) >> "$LOG_FILE" 2>&1
+if [[ -z "$base_img" || "$1" == "re_base" ]]; then
+  echo -e "\e[48;5;172m Building bsae image! \e[0m"
+  cd base && ./build.sh && cd .. >> "$LOG_FILE"
 fi
+echo -e "\e[48;5;172m bsae image will not be build! \e[0m"
 # shellcheck disable=SC2129
-(cd jetbot && ./build.sh) >> "$LOG_FILE" 2>&1
+cd jetbot && ./build.sh && cd .. >> "$LOG_FILE"
 # cd models && ./build.sh && cd .. >> $LOG_FILE
-(cd display && ./build.sh) >> "$LOG_FILE" 2>&1
-(cd jupyter && ./build.sh) >> "$LOG_FILE" 2>&1
-(cd camera && ./build.sh) >> "$LOG_FILE" 2>&1
+cd display && ./build.sh && cd .. >> "$LOG_FILE"
+cd jupyter && ./build.sh && cd .. >> "$LOG_FILE"
+cd camera && ./build.sh &&  cd .. >> "$LOG_FILE"
 
 ./disable.sh
 docker image prune -f
