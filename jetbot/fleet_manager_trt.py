5bfa9f0e239026df75f02243a7b6e5b434922626
Cuter <cuter9@msn.com>
2026-09-01 22:45:58 +0800

    a new fleet control with only one model; flee control

--- jetbot/fleet_manager_trt.py
+++ jetbot/fleet_manager_trt.py
@@ -173,8 +173,8 @@ class FleeterTRT(ObjectFollower, RoadCruiserTRT):
             center = object_center_detection(cls_obj)
             print(f"closest object bounding box: {cls_obj["bbox"]}; center of object: {center}")
             self.robot.set_motors(
-                float(self.speed_fm + self.turn_gain_fm * center[0] + self.steering_bias_fm),
-                float(self.speed_fm - self.turn_gain_fm * center[0] + self.steering_bias_fm)
+                float(self.speed_fm + self.turn_gain_fm * center[0] + self.steering_bias_fm),   # left motor
+                float(self.speed_fm - self.turn_gain_fm * center[0] + self.steering_bias_fm)    # right motor
             )
 
         # update image widget
