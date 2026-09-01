09069e143baa777a6fb6b574ce3b010edfbf8970
Cuter <cuter9@msn.com>
2026-09-02 02:10:04 +0800

    a new fleet control with only one model;

--- jetbot/robot.py
+++ jetbot/robot.py
@@ -51,6 +51,7 @@ class Robot(SingletonConfigurable):
     # config
     i2c_bus = traitlets.Integer(default_value=1).tag(config=True)
     left_motor_channel = traitlets.Integer(default_value=1).tag(config=True)
+    # set maximum output power limit (alpha) of motor
     # left_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
     left_motor_alpha = traitlets.Float(default_value=0.8).tag(config=True)
     right_motor_channel = traitlets.Integer(default_value=2).tag(config=True)
