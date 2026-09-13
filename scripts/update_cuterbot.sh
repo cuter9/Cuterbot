3c7b153fcb48d009bc33f69ee82d7f5841fc3691
Cuter <cuter9@msn.com>
2026-09-13 18:39:32 +0800

    revise and delete older scripts

--- scripts/update_cuterbot.sh
+++ scripts/update_cuterbot.sh
@@ -23,7 +23,7 @@ if [ ! -d ${HOME}/model_repo ]; then
   echo "Downloading models ------- "
   ${HOME}/Cuterbot/scripts/download_model_repo.sh
 else
-  echo -e "\n\e[48;5;172m Skip downloading models! Check the models is already in /home/cuterbot/Cuterbot/model_repo !\e[0m"
+  echo -e "\n\e[48;5;172m Skip downloading models! Check the models is already in /home/cuterbot/Cuterbot/model_repo !\e[0m\n"
 fi
 
 
