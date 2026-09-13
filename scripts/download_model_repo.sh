3084ccdc5028d2d2ebe2cf4cf237189b7a7ab4eb
Cuter <cuter9@msn.com>
2026-09-13 18:04:19 +0800

    revise and delete older scripts

--- scripts/download_model_repo.sh
+++ scripts/download_model_repo.sh
@@ -1,7 +1,5 @@
 sudo apt install -y python3-pip
 sudo pip3 install -U pip gdown
 sudo chown -R $(whoami) $HOME/.cache/
-# sudo cp ./gdrive_model_repo_cookies.json $HOME/.cache/gdown/cookies.json
-# sudo cp ./gdrive_model_repo_cookies.txt $HOME/.cache/gdown/cookies.txt
-# gdown --no-cookies --folder https://drive.google.com/drive/folders/1-RIQ6lSdFI94IMlZbnRPMQHcG4L8t9Ss -O ${HOME}/model_repo
+
 gdown --no-cookies --folder https://drive.google.com/drive/folders/1l_PPNVJC2VVXZxv0N63uJhoBWndeIBaC -O ${HOME}/model_repo
