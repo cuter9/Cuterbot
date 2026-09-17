e3d5258d2715a6a8551ce84fc6792a6ca3e03f7c
Cuter <cuter9@msn.com>
2026-09-18 01:23:20 +0800

    revise and delete older scripts

--- jetbot/data_collection.py
+++ jetbot/data_collection.py
@@ -16,6 +16,8 @@ class DataCollection(HasTraits):
         self.height_display = self.camera.height_display
         self.widget_image = np.empty((self.height_display, self.width_display, 3), dtype=np.uint8).tobytes()
         self.xy_image = np.empty((self.height_display, self.width_display, 3), dtype=np.uint8).tobytes()
+        self.xy_point_image = np.empty((self.height_display, self.width_display, 3), dtype=np.uint8).tobytes()
+        self.bbox_image =  np.empty((self.height_display, self.width_display, 3), dtype=np.uint8).tobytes()
 
     def get_xy_image(self, x_slider_value, y_slider_value):
         image = cv2.resize(self.camera.value, (self.width_display, self.height_display),
@@ -30,6 +32,27 @@ class DataCollection(HasTraits):
         image = cv2.line(image, (0, int(self.height_display / 2)), (int(self.width_display), int(self.height_display / 2)),
                          (0, 255, 255), 2)
         self.xy_image = bgr8_to_jpeg(image)
+
+    def get_xy_point_image(self, x_slider_value, y_slider_value):
+        image = cv2.resize(self.camera.value, (self.width_display, self.height_display),
+                           interpolation=cv2.INTER_LINEAR)
+        x = x_slider_value
+        y = y_slider_value
+        x = int(x * self.width_display / 2 + self.width_display / 2)
+        y = int(y * self.height_display / 2 + self.height_display / 2)
+        image = cv2.circle(image, (x, y), 4, (0, 255, 0), 2)
+        image = cv2.circle(image, (int(self.width_display / 2), int(self.height_display)), 4, (0, 0, 255), 2)
+        self.xy_point_image = bgr8_to_jpeg(image)
+
+    def get_bbox_image(self, bbox):
+        image = cv2.resize(self.camera.value, (self.width_display, self.height_display),
+                           interpolation=cv2.INTER_LINEAR)
+        x1 = bbox[0][0] * self.width_display / 2 + self.width_display / 2
+        y1 = bbox[0][1] * self.height_display / 2 + self.height_display / 2
+        x2 = bbox[1][0] * self.width_display / 2 + self.width_display / 2
+        y2 = bbox[1][1] * self.height_display / 2 + self.height_display / 2
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        self.bbox_image = bgr8_to_jpeg(image)
 
     def get_widget_image(self):
         self.widget_image = bgr8_to_jpeg(cv2.resize(self.camera.value, (self.width_display, self.height_display),
