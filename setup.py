d2a51cf2c4b324e02b85389bcc08d82158f3ef73
Cuter <cuter9@msn.com>
2026-09-18 09:11:30 +0800

    revise and delete older scripts

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     description='An open-source robot based on NVIDIA Jetson Nano',
     packages=find_packages(),
     install_requires=[
+        'numpy<=3.5',
         'Adafruit_MotorHat',
         'Adafruit-SSD1306',
     ],
